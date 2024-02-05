a8a093dc14d2b8741e13b421ce4743abc59e84b2
Rania Chouchene <rania.chouchene.2019@gmail.com>
2024-02-05 21:22:28 +0100

    add hook-form

--- src/LogIn/LogIn.tsx
+++ src/LogIn/LogIn.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
 import "./Login.css";
 import { loginUser } from "../services/UserService";
-
+import { useForm, SubmitHandler } from "react-hook-form";
+interface IFormInput {
+  username: string;
+  password: string;
+}
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = () => {
-    loginUser(username, password)
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+    loginUser(data.username, data.password)
       .then((response) => response.json())
       .then((json) => console.log(json));
   };
@@ -22,30 +24,18 @@ function Login() {
       <div className="form-container">
         <div className="login-container">
           <h2>LogIn</h2>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor="username">
               <FontAwesomeIcon icon={faUser} /> UserName:
             </label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+            <input type="text" id="username" {...register("username")} />
 
             <label htmlFor="password">
               <FontAwesomeIcon icon={faLock} /> PassWord:
             </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <input type="password" id="password" {...register("password")} />
 
-            <button type="button" onClick={handleLogin}>
-              LOGIN
-            </button>
+            <button type="submit">LOGIN</button>
             <p>
               Pas de compte ?{" "}
               <a href="/inscription" className="create-account-link">
